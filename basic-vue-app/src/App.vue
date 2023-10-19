418ef7f7f7c7e1e28f25f121fa2328b2d34d7f41
zhoujie <skywalk94@foxmail.com>
2023-10-19 09:01:14 +0800

    bug：修改主应用配置的微应用路径

--- basic-vue-app/src/App.vue
+++ basic-vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <router-link to="/subVue">主应用与vue微应用</router-link>
+  <router-link to="/micro-vue">主应用与vue微应用</router-link>
   -----------------------------
-  <router-link to="/subReact">主应用与react微应用</router-link>
+  <router-link to="/micro-react">主应用与react微应用</router-link>
   <br />
   <router-view />
   <div id="vue-app-container" />
